abd07e6a2a954b4ae3c5b39188d2c081419b5d91
Majd Alfhaily <majd@alfhaily.me>
2018-10-15 10:01:44 +0200

    Added filter to Active Customers diagram

--- src/components/app/diagrams/ActiveCustomers.vue
+++ src/components/app/diagrams/ActiveCustomers.vue
@@ -6,17 +6,42 @@
 <script>
 
 export default {
+  props: {
+    selected: {
+      type: Object
+    }
+  },
+  watch: {
+    selected: {
+      handler: function (val) {
+        this.requestData.from = val.startDate.toISOString()
+        this.requestData.to = val.endDate.toISOString()
+        this.apiRequest()
+      },
+      deep: true
+    }
+  },
+  data () {
+    return {
+      requestData: {
+        customerId: 'c1',
+        from: this.selected.startDate,
+        to: this.selected.endDate,
+        dataPointCount: 1
+      },
+    }
+  },
   mounted () {
-    // this.$api.get('visitors/count')
-    // .then( (response) => {
-    //   const total = response.data.total;
-    //   this.$emit('updateSubtitle', total)
-    // })
-    // .catch(function (error) {
-    //   console.log(error);
-    // })
-    // .then(function () {
-    // });
+    this.apiRequest();
+  },
+  methods: {
+    apiRequest () {
+      this.$api.post('visitors/count', this.requestData)
+      .then((res) => {
+        const total = res.data.total;
+        this.$emit('updateSubtitle', total)
+      });
+    }
   }
 }
 </script>
