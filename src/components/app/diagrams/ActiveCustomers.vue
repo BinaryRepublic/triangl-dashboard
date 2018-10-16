5cecbd9d74a6040e44e96685ca82609577b410d4
Tobias Kemkes <kemkes-tobias@web.de>
2018-10-16 13:58:16 +0200

    Fixed Filter

--- src/components/app/diagrams/ActiveCustomers.vue
+++ src/components/app/diagrams/ActiveCustomers.vue
@@ -4,29 +4,18 @@
 </template>
 
 <script>
-
 export default {
   props: {
-    selected: {
+    selectedActive: {
       type: Object
-    }
-  },
-  watch: {
-    selected: {
-      handler: function (val) {
-        this.requestData.from = new Date(val.startDate.getFullYear(), val.startDate.getMonth(), val.startDate.getDate() - 1).toISOString()
-        this.requestData.to = new Date(val.endDate.getFullYear(), val.endDate.getMonth(), val.endDate.getDate() - 1, 23, 59, 59).toISOString()
-        this.apiRequest()
-      },
-      deep: true
     }
   },
   data () {
     return {
       requestData: {
         customerId: 'c1',
-        from: this.selected.startDate,
-        to: this.selected.endDate,
+        from: this.selectedActive.startDate,
+        to: this.selectedActive.endDate,
         dataPointCount: 1
       }
     }
