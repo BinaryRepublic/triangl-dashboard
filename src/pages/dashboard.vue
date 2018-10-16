186d6b7bd551bd375d9fba44a51637965c919a20
Tobias Kemkes <kemkes-tobias@web.de>
2018-10-16 13:58:32 +0200

    Fixed Filter Active

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -4,7 +4,7 @@
       <DatePicker @selected="selected" class="datePicker" format="YY MM DD HH mm ss"></DatePicker>
       <div class="clear"></div>
       <Canvas componentSize="two" componentName="CountCustomers" title="Customers" subtitle="..." :selected="selectedObj" />
-      <Canvas componentSize="one" componentName="ActiveCustomers" title="Active Customers" subtitle="..." :selected="selectedObj" type="blue" />
+      <Canvas componentSize="one" componentName="ActiveCustomers" title="Active Customers" subtitle="..." :selectedActive="selectedObjActive" type="blue" />
       <Canvas componentSize="one" componentName="PeakHours" title="Peak Hours" :selected="selectedObj"/>
       <Canvas componentSize="two" componentName="Map" title="Heat Map" :selected="selectedObj"/>
     </div>
@@ -16,9 +16,11 @@ import Canvas from '../components/app/elements/Canvas.vue'
 import DatePicker from '../components/app/elements/DatePicker.vue'
 
 var n = new Date()
+console.log(n)
 
 export default {
   mounted () {
+    this.test()
   },
   components: {
     Canvas,
@@ -29,10 +31,18 @@ export default {
       selectedObj: {
         startDate: new Date(n.getFullYear(), n.getMonth(), n.getDate()).toISOString(),
         endDate: new Date(n.getFullYear(), n.getMonth(), n.getDate(), 23, 59, 59).toISOString()
+      },
+      selectedObjActive: {
+        startDate: new Date(n.getFullYear(), n.getMonth(), n.getDate(), n.getHours() - 1, n.getMinutes(), n.getSeconds(), n.getMilliseconds()).toISOString(),
+        endDate: n.toISOString()
       }
     }
   },
   methods: {
+    test () {
+      console.log(this.selectedObjActive.startDate)
+      console.log(this.selectedObjActive.endDate)
+    },
     selected (e) {
       console.log(e)
       this.selectedObj.startDate = e.start
