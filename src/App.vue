b202c459cc7db7636843956acb0169aa4340f12b
Tobias Kemkes <kemkes-tobias@web.de>
2018-10-24 14:54:31 +0200

    Deleted Basic Login

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,13 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace>Logout</router-link>
-    </div>
-    <router-view @authenticated="setAuthenticated"/>
+    <router-view/>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App',
-  components: {
-
-  },
-  data () {
-    return {
-      authenticated: false,
-      mockAccount: {
-        username: 'majd',
-        password: 'winteriscoming'
-      }
-    }
-  },
-  mounted () {
-    if (!this.authenticated) {
-      this.$router.replace({ name: 'login' })
-    }
-  },
-  methods: {
-    setAuthenticated (status) {
-      this.authenticated = status
-    },
-    logout () {
-      this.authenticated = false
-    }
-  }
+  name: 'App'
 }
 </script>
 
