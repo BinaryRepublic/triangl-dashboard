e26f2e8e572564710f8bf0c5e26aff57d8880a9b
Tobias Kemkes <kemkes-tobias@web.de>
2018-11-01 12:32:48 +0100

    Fixed Filter Allignment

---
+++ src/components/dashboard/DashboardCanvasTemplate/DashboardCanvasTemplate.vue
@@ -0,0 +1,72 @@
+<template>
+  <section class="module" :class="getClasses()">
+    <p class="title">{{title}}</p>
+    <p class="subtitle">{{subtitle}}</p>
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+
+export default {
+  props: {
+    componentSize: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      default: 'white'
+    },
+    title: {
+      type: String
+    },
+    subtitle: {
+      type: String
+    }
+  },
+  methods: {
+    getClasses () {
+      return {
+        'one': this.componentSize === 'one',
+        'two': this.componentSize === 'two',
+        'three': this.componentSize === 'three',
+        'blue': this.type === 'blue'
+      }
+    }
+  }
+}
+</script>
+<style lang="less">
+  .module {
+    float: left;
+    height: 420px;
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .2), 0 1px 1px 0 rgba(0, 0, 0, .14), 0 2px 1px -1px rgba(0, 0, 0, .12);
+    border-radius: 2px;
+    margin: 10px;
+    background-color: #ffffff;
+    padding: 20px;
+
+    &.one {
+      width: calc(33.3333333% - 20px);
+    }
+    &.two {
+      width: calc(66.666666666% - 20px);
+    }
+    &.three {
+      width: calc(100% - 20px);
+    }
+    &.blue {
+      background-color: #0da8f9;
+      color: white;
+    }
+    .title {
+      font-size: 13px;
+      margin-bottom: 5px;
+    }
+    .subtitle {
+      font-size: 22px;
+      margin-bottom: 20px;
+    }
+  }
+</style>
