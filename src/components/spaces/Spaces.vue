7430ed0c63106940ff25ff689d6261852d6376ca
Tobias Kemkes <kemkes-tobias@web.de>
2018-12-03 11:05:41 +0100

    Add modules

--- src/components/spaces/Spaces.vue
+++ src/components/spaces/Spaces.vue
@@ -1,12 +1,26 @@
 <template>
-  <div>
-    Spaces
+  <div class="container-content">
+    <Module componentWidth="one" componentHeight="one">
+      Spock <> Paper
+    </Module>
+    <Module componentWidth="one" componentHeight="one">
+      Spock <> Rock
+    </Module>
+    <Module componentWidth="one" componentHeight="one">
+      Scissors <> Team Room
+    </Module>
   </div>
 </template>
 
 <script>
+import Module from '../shared/ModuleTemplate/ModuleTemplate'
 
+export default {
+  components: {
+    Module
+  }
+}
 </script>
-<style lang="less">
 
+<style lang="less" scoped>
 </style>
